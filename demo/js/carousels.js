178d862c444b1627688ee2a44acb48491e9e0bd3
zda <zach@zachalexander.com>
2014-02-19 19:56:52 -0800

    Fix quoteWidth bug; minor cleanup

--- demo/js/carousels.js
+++ demo/js/carousels.js
@@ -1,31 +1,33 @@
-$(function(){
-  // vars for testimonials carousel
-  var $txtcarousel = $('#testimonial-list');
-  var txtcount = $txtcarousel.children().length;
-  var quoteWidth = $('.carousel-wrap li').get(0).css("width");
-  var wrapwidth = (txtcount * quoteWidth) + quoteWidth; // 700px width for each testimonial item
-  $txtcarousel.css('width',wrapwidth);
-  var animtime = 750; // milliseconds for clients carousel
-  var fadetime = 500;
- 
+$(function () {
+  // vars for carousel
+  var $textCarousel = $('#testimonial-list');
+  var textCount = $textCarousel.children().length;
+  var quoteWidth = $('.carousel-wrap li').first().width();
+  console.log(quoteWidth);
+  var wrapWidth = (textCount * quoteWidth) + quoteWidth;
+  console.log(wrapWidth);
+  $textCarousel.css('width', wrapWidth);
+  var animationTime = 750; // milliseconds for clients carousel
+  var fadeTime = 500;
+
   // prev & next btns for testimonials
   $('#prv-testimonial').on('click', function(){
     var $last = $('#testimonial-list li:last');
     $last.remove().css({ 'margin-left': '-1106px' });
     $('#testimonial-list li:first').before($last);
-    $last.fadeOut(fadetime, function(){
-    	//$last.animate({ 'margin-left': '0px' }, animtime, 'swing', function(){
+    $last.fadeOut(fadeTime, function(){
+    	//$last.animate({ 'margin-left': '0px' }, animationTime, 'swing', function(){
     	//	$last.css('opacity', '1');
     	//});
-    }); 
+    });
   });
-  
+
   $('#nxt-testimonial').on('click', function(){
     var $first = $('#testimonial-list li:first');
-    $first.animate({ 'margin-left': '-1106px' }, animtime, function() {
+    $first.animate({ 'margin-left': '-1106px' }, animationTime, function() {
       $first.remove().css({ 'margin-left': '0px' });
       $('#testimonial-list li:last').after($first);
-    });  
+    });
   });
-  
+
 });
